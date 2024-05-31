f0c166f8f302f0475d7af29edb391f9855416ae3
Ahmed Eissa <eissamail@googlemail.com>
2024-05-31 16:42:29 +0100

    changed the code in the questions files.

--- Question1.php
+++ Question1.php
@@ -1,3 +1,21 @@
 <?php
+set_error_handler(
+    function ($severity, $message, $file, $line)
+    {
+        throw new ErrorException($message, $severity, $severity, $file, $line);
+    }
+);
 
+//get passed in arguments
+$csvFilePath = $argv[1];
+
+//return the file contents if it exists
+try
+{
+    echo file_get_contents($csvFilePath).PHP_EOL;
+}
+catch (Exception $ex)
+{
+    echo "File not found: ".trim($ex->getMessage()).PHP_EOL;
+}
 ?>
--- Question2.php
+++ Question2.php
@@ -1,4 +1,20 @@
 <?php
+/*
+ * x mod 3 = Foo
+ * x mod 5 = Bar
+ * x mod 15 = FooBar
+ */
 
+for ($x = 1; $x <= 100; $x++){
+    if($x % 15 ==0){
+        echo "foobar,\t";
+    } elseif($x % 5 ==0) {
+        echo "bar,\t";
+    } elseif($x % 3 ==0){
+        echo "foo,\t";
+    } else {
+        echo $x . ",\t";
+    }
+}
 
 ?>
